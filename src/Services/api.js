805f74536343334549e9ca8b4a3a3773f4f61ca9
thamaesamuel <thamaesamuel@gmail.com>
2026-06-02 18:36:59 +0200

    Deployment ursl change

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:4000/api",
+//  baseURL: "https://localhost:4000/api",
+  baseURL:"https://loanshark-phi.vercel.app/",
   withCredentials: true,
 });
 
