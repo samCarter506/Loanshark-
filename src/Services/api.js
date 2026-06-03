cbabf0813fcb3fa9305ba39c8ca7cf234771a232
thamaesamuel <thamaesamuel@gmail.com>
2026-06-03 15:46:49 +0200

    update api

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //
-  baseURL:"https://loansharkapi.onrender.com/api",
+  baseURL:"https://loanshark-phi.vercel.app/api",
   withCredentials: true,
 });
 
