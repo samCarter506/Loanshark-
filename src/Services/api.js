0b0116cd1a3c9d46619ac1943d36d5d15bf53e9d
thamaesamuel <thamaesamuel@gmail.com>
2026-06-05 09:54:06 +0200

    update of api

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   //api
-  //baseURL:"https://loansharkapi.onrender.com/api",
-  baseURL:"https://localhost:4000/api",
+  baseURL:"https://loansharkapi.onrender.com/api",
+  //baseURL:"https://localhost:4000/api",
 
   withCredentials: true,
 });
